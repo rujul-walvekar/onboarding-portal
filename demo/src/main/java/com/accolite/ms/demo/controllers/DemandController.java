eea1af3f16d2359302b5fb746ecd96c710dedd68
Rujul Walvekar <rujul.w@gmail.com>
2021-04-26 22:42:05 +0530

    draft-controllers-demand-and-admin

---
+++ demo/src/main/java/com/accolite/ms/demo/controllers/DemandController.java
@@ -0,0 +1,14 @@
+package com.accolite.ms.demo.controllers;
+
+import com.accolite.ms.demo.entities.DemandEnt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class DemandController {
+
+}
