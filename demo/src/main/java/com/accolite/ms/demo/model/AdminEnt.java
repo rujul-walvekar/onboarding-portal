892b23ccf3aa4eb9b6fd303d0800a4c747af5906
Rujul Walvekar <rujul.w@gmail.com>
2021-04-30 18:03:23 +0530

    entities->model updated

---
+++ demo/src/main/java/com/accolite/ms/demo/model/AdminEnt.java
@@ -0,0 +1,45 @@
+package com.accolite.ms.demo.model;
+
+public class AdminEnt {
+
+    private String adminName;
+    // private Integer id;
+    private Integer mobileNumber;
+    private Integer adminID;
+
+
+    public String getAdminName() {
+        return this.adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    // public Integer getId() {
+    //     return this.id;
+    // }
+
+    // public void setId(Integer id) {
+    //     this.id = id;
+    // }
+
+    public Integer getMobileNumber() {
+        return this.mobileNumber;
+    }
+
+    public void setMobileNumber(Integer mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Integer getAdminID() {
+        return this.adminID;
+    }
+
+    public void setAdminID(Integer adminID) {
+        this.adminID = adminID;
+    }
+    
+}
+
+    
---
+++ demo/src/main/java/com/accolite/ms/demo/model/DemandEnt.java
@@ -0,0 +1,35 @@
+package com.accolite.ms.demo.model;
+
+public class DemandEnt{
+
+    // private Integer id;
+    private Integer demandID;
+    // private Integer adminID;
+
+    // public Integer getId() {
+    //     return this.id;
+    // }
+
+    // public void setId(Integer id) {
+    //     this.id = id;
+    // }
+
+    public Integer getDemandID() {
+        return this.demandID;
+    }
+
+    public void setDemandID(Integer demandID) {
+        this.demandID = demandID;
+    }
+
+    // public Integer getAdminID() {
+    //     return this.adminID;
+    // }
+
+    // public void setAdminID(Integer adminID) {
+    //     this.adminID = adminID;
+    // }
+
+    
+
+}
