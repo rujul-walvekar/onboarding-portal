dc6bcfc0d87bb41f06ef8913bc262eececdd755a
Rujul Walvekar <rujul.w@gmail.com>
2021-05-02 03:35:10 +0530

    removed unused libraries

--- demo/src/main/java/com/accolite/ms/demo/controllers/AddDetails.java
+++ demo/src/main/java/com/accolite/ms/demo/controllers/AddDetails.java
@@ -2,10 +2,8 @@ package com.accolite.ms.demo.controllers;
 
 import com.accolite.ms.demo.model.OnboardeeEnt;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200") //for angular
@@ -18,12 +16,7 @@ public class AddDetails {
     @PostMapping("/addOnboardeeDetails")
     public int addOnboardeeDetails(@RequestBody OnboardeeEnt onboardeeEnt) {
         String sqlQuery = "INSERT INTO board_details(age, company, location, emailID) VALUES(?, ?, ?, ?)";
-        try {
         return jdbcTemplate.update(sqlQuery, onboardeeEnt.getAge(), onboardeeEnt.getCompany(), onboardeeEnt.getLocation(), onboardeeEnt.getEmailID());
-        } catch (Exception e) {
-        System.out.println("Exception\t" + e.toString());
-        return 0;
-        }
         }
         
 }   
